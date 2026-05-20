eacd3462d79b41e350f76d94042223850bac6008
Tanusha Banarsee <tban0029@student.monash.edu>
2026-05-20 09:05:28 +0800

    Add DV2 Malaysia Tourism visualisation draft

---
+++ js/vega-lite-vis.js
@@ -0,0 +1,39 @@
+vegaEmbed("#recovery-index-line", "vis/recovery-index-line.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#expenditure-waterfall", "vis/expenditure-waterfall.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#state-lollipop", "vis/state-lollipop.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#monthly-heatmap", "vis/monthly-heatmap.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#source-market-bump", "vis/source-market-bump.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#source-market-map", "vis/source-market-map.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#state-scatter", "vis/state-scatter.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#domestic-radial", "vis/domestic-radial.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#state-radar", "vis/state-radar.vg.json", {
+  actions: false
+}).catch(console.error);
+
+vegaEmbed("#state-hotel-map", "vis/state-hotel-map.vg.json", {
+  actions: false
+}).catch(console.error);
